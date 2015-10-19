e08f3a0a1ade34adc71623f8d7b3e97066e89c7a
Raabb Ajam <raabbajam@gmail.com>
2015-10-19 22:43:40 +0700

    Moved server data

--- app.js
+++ app.js
@@ -6,90 +6,96 @@ var
   express = require('express'),
   bodyParser = require('body-parser'),
   vhost = require('vhost'),
-  hosts = require('./vhost'),
-  api = require('./api'),
-  app = express()
+  hosts = require('akura.co/vhost'),
+  app = module.exports = express()
 
-// collect aliases
-// hosts = hosts.reduce(function (hosts, host) {
-//   var alias
-//   try {
-//     alias = require(host + '/alias')
-//   } catch (e) {
-//     alias = []
-//   }
-//   [host].concat(alias).forEach(function (name) {
-//     if (!hosts[name]) {
-//       hosts[name] = host
-//     }
-//   })
-//   return hosts
-// }, {})
 process.env.HOME = '/home/ubuntu'
 app.
   use(bodyParser.json()).
-  use(bodyParser.urlencoded({extended: true})).
-  use(api).
-  use(function (req, res, next) {
-    if (req.hostname == 'akura.co') {
-      if (!req.secure)
-        return res.redirect('https://' + req.hostname + req.url)
-      res.set('Strict-Transport-Security', 'max-age=86400')
-    }
-    next()
-  })//.
-  // use('/telegram/quoteBot/hook', require(process.env.HOME + '/quoteBot/app.js')())
+  use(bodyParser.urlencoded({extended: true}))
+if (process.env.NODE_ENV === 'development') {
+  app.use(require('errorhandler')())
 
-// _.each(hosts, function (domain, repo) {
-//   if (['akura.co'].indexOf(domain) > -1)
-//     return app.use(vhost(domain, express().use(express.static(__dirname + '/' + domain))))
-//   return app.use(vhost(domain, require(domain)))
-// })
-var requireApp = function (host) {
+  _.each(hosts, function (host) {
+    var
+      vhostApp = requireApp(host),
+      alias
+    // app.use('/' + host, express.static('node_modules/' + host + '/public/') )
+    app.use('/' + host, vhostApp)
+    try {alias = require(host + '/alias')} catch (e) {}
+    _.each(alias, function (alias) {
+      // app.use('/' + alias, express.static('node_modules/' + host + '/public/') )
+      app.use('/' + alias, vhostApp)
+    })
+  })
+  app.get('*', function(req, res, next) {
+    res.
+      status(404).
+      send('Not found')
+  })
+  if (!module.parent) {
+    app.listen(3000)
+  }
+
+} else {
+  app.
+    use(function (req, res, next) {
+      if (req.hostname == 'akura.co') {
+        if (!req.secure)
+          return res.redirect('https://' + req.hostname + req.url)
+        res.set('Strict-Transport-Security', 'max-age=86400')
+      }
+      next()
+    }).
+    use('/telegram/quoteBot/hook', require(process.env.HOME + '/quoteBot/app.js')())
+
+  _.each(hosts, function (host) {
+    var
+      vhostApp = requireApp(host),
+      alias
+    app.use(vhost(host, vhostApp))
+    try {alias = require(host + '/alias')} catch (e) {}
+    _each(alias, function (alias) {
+      app.use(vhost(alias, vhostApp))
+    })
+  })
+  http.createServer(app).listen(80)
+  https.createServer({
+    key: fs.readFileSync(process.env.HOME + '/.akura.co/ssl/akura.co.key'),
+    cert: fs.readFileSync(process.env.HOME + '/.akura.co/ssl/akura.co.crt'),
+    ca: [
+      fs.readFileSync(process.env.HOME + '/.akura.co/ssl/rapidSsl.crt'),
+    ],
+    honorCipherOrder: true,
+    ciphers: [
+      'ECDHE-RSA-AES256-SHA384',
+      'DHE-RSA-AES256-SHA384',
+      'ECDHE-RSA-AES256-SHA256',
+      'DHE-RSA-AES256-SHA256',
+      'ECDHE-RSA-AES128-SHA256',
+      'DHE-RSA-AES128-SHA256',
+      'HIGH',
+      '!aNULL',
+      '!eNULL',
+      '!EXPORT',
+      '!DES',
+      '!RC4',
+      '!MD5',
+      '!PSK',
+      '!SRP',
+      '!CAMELLIA'
+    ].join(':')
+  }, app).listen(443)
+}
+
+function requireApp (host) {
   //support both app and static
   var app
   try {
     app = require(host)
   } catch (e) {
+    console.error(host, e.stack);
     app = express.static(__dirname + '/node_modules/' + host)
   }
   return app
 }
-_.each(config.host, function (host) {
-  var vhostApp = requireApp(host)
-  app.use(vhost(host, vhostApp))
-  var alias
-  try {alias = require(host + '/alias')} catch (e) {}
-  _each(alias, function (alias) {
-    app.use(vhost(alias, vhostApp))
-  })
-})
-app.listen(3000)
-return;
-http.createServer(app).listen(80)
-https.createServer({
-  key: fs.readFileSync(process.env.HOME + '/.akura.co/ssl/akura.co.key'),
-  cert: fs.readFileSync(process.env.HOME + '/.akura.co/ssl/akura.co.crt'),
-  ca: [
-    fs.readFileSync(process.env.HOME + '/.akura.co/ssl/rapidSsl.crt'),
-  ],
-  honorCipherOrder: true,
-  ciphers: [
-    'ECDHE-RSA-AES256-SHA384',
-    'DHE-RSA-AES256-SHA384',
-    'ECDHE-RSA-AES256-SHA256',
-    'DHE-RSA-AES256-SHA256',
-    'ECDHE-RSA-AES128-SHA256',
-    'DHE-RSA-AES128-SHA256',
-    'HIGH',
-    '!aNULL',
-    '!eNULL',
-    '!EXPORT',
-    '!DES',
-    '!RC4',
-    '!MD5',
-    '!PSK',
-    '!SRP',
-    '!CAMELLIA'
-  ].join(':')
-}, app).listen(443)
